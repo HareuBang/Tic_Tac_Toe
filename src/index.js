ec194fae9980d33b2dd930c3ee4a270d352353c7
HareuBang <divjae7124@gmail.com>
2024-09-05 11:50:48 +0900

    github Pages 주소

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename="Tic_Tac_Toe">
     <App />
   </React.StrictMode>
 );
